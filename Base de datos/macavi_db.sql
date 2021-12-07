c7a3d7689d7d6cd7dbde0758a3af1b24e9126338
Victor-Delg <Vicsdelgado@misena.edu.co>
2021-12-06 22:19:06 -0500

    update

--- Base de datos/macavi_db.sql
+++ Base de datos/macavi_db.sql
@@ -1,4 +1,5 @@
 
+
 CREATE TABLE macavi.cliente (
   id         serial NOT NULL, 
   id_locate  int4 NOT NULL, 
@@ -93,3 +94,87 @@ ALTER TABLE macavi.rol_usuario ADD CONSTRAINT fk_rol_usua FOREIGN KEY (rolid) RE
 ALTER TABLE macavi.usuario ADD CONSTRAINT fk_usu_dni FOREIGN KEY (id_dni) REFERENCES macavi.tipo_dni (id) ON UPDATE No action ON DELETE No action;
 ALTER TABLE macavi.cliente ADD CONSTRAINT fk_usua_clie FOREIGN KEY (id_usuario) REFERENCES macavi.usuario (id);
 ALTER TABLE macavi.rol_usuario ADD CONSTRAINT fk_usua_rol FOREIGN KEY (usuarioid) REFERENCES macavi.usuario (id);
+
+/*1 consultar las facturas de un cliente por su tipo_dni y numero_dni*/
+SELECT * FROM  macavi.tipo_dni td INNER JOIN macavi.usuario u ON td.id = u.id_dni
+INNER JOIN macavi.cliente cl on u.id = cl.id_usuario
+INNER JOIN macavi.factura f ON cl.id = f.id_cliente
+WHERE td.nombre_dni = 'nit' AND u.numero_dni = '00'; 
+
+/*2 consultar los clientes por locate y numero_dni*/
+SELECT * FROM  macavi.tipo_dni td INNER JOIN macavi.usuario u ON td.id = u.id_dni
+INNER JOIN macavi.cliente cl on u.id = cl.id_usuario
+INNER JOIN macavi.locate lo ON lo.id = cl.id_locate
+WHERE u.id_dni = '0' AND cl.id_locate = '1';
+
+/*3 consultar los usuarios por rol*/
+SELECT * FROM  macavi.rol r INNER JOIN macavi.rol_usuario ru ON r.id = ru.rolid
+INNER JOIN macavi.usuario u on u.id = ru.usuarioid
+WHERE r.id  = '1' AND u.id = '1';
+
+/*4 consultar las facturas por producto facturado*/
+SELECT * FROM  macavi.producto p INNER JOIN macavi.producto_factura pf ON p.id = pf.id_producto
+INNER JOIN macavi.factura f on f.id = pf.id_factura
+WHERE f.id  = '1' AND p.id = '1';
+
+/*5 consultar las facturas por usuario*/
+SELECT * FROM  macavi.usuario u INNER JOIN macavi.factura f ON u.id = f.id_usuario
+WHERE u.id  = '1';
+
+/*6 consultar las facturas por locate*/
+SELECT * FROM  macavi.locate l INNER JOIN macavi.cliente cl ON l.id = cl.id_locate
+INNER JOIN macavi.factura f ON cl.id = f.id_cliente
+WHERE cl.id_locate  = '1' and f.id_cliente = '1';
+
+/*7 consultar cliente por email*/
+SELECT * FROM  macavi.usuario u INNER JOIN macavi.cliente cl ON u.id = cl.id_usuario
+WHERE u.email  = '' and cl.id = '1'; 
+
+/*8 consultar usuario por tipo dni*/
+SELECT * FROM  macavi.tipo_dni td INNER JOIN macavi.usuario u ON td.id = u.id_dni
+WHERE u.id_dni  = '1' and u.id = '1';
+
+/*9 consultar cliente por ciudad*/
+SELECT * FROM  macavi.locate l INNER JOIN macavi.cliente cl ON l.id = cl.id_locate
+WHERE l.pais  = '' and cl.id = '1';
+
+/*9 consultar cliente por ciudad*/
+SELECT * FROM  macavi.locate l INNER JOIN macavi.cliente cl ON l.id = cl.id_locate
+WHERE l.pais  = '' and cl.id = '1';
+
+/*10 consultar cliente por dni*/
+SELECT * FROM  macavi.tipo_dni td INNER JOIN macavi.usuario u ON td.id = u.id_dni
+INNER JOIN macavi.cliente cl on u.id = cl.id_usuario
+WHERE cl.id = '1' AND u.numero_dni = '00'; 
+
+INSERT INTO macavi.locate(
+	pais, departamento, ciudad)
+	VALUES ('Colombia','Cundinamarca','soacha');
+    
+INSERT INTO macavi.rol(
+	id,nombre_rol)
+	VALUES (1,'ADMIN'),(2,'CLIENTE');
+
+INSERT INTO macavi.producto(
+	id, descripcion_producto, marca, genero, talla, estilo, cantidad_producto, porcentaje_iva)
+	VALUES (1,'zapatilla color negro con naranja','JORDAN','MASCULINO',39,'RETRO 6',10, 0.19);
+
+INSERT INTO macavi.tipo_dni(
+	id, nombre_dni)
+	VALUES (1,'NIT'),(2,'CEDULA CIUDADANIA'),(3,'PASAPORTE');
+
+INSERT INTO macavi.usuario(
+	id,id_dni, numero_dni, nombre, login_usuario, password, email)
+	VALUES (1,2,'1003804101','DEISON MAÑOSCA','Dmañosca','deisonperroloco123','deisonperroloco@gmail.com');
+
+INSERT INTO macavi.factura(
+	id, id_usuario, id_cliente, fecha_fact, fecha_venc, total_factura, descripcion, tipo_pago)
+	VALUES (1, 1, 1, '14-11-2021','15-11-2021',170000,'mala paga','credito');
+
+INSERT INTO macavi.cliente(
+	id, id_locate, id_usuario, direccion, telefono)
+	VALUES (1,1,1,'leon 13',3212);
+
+
+
+
