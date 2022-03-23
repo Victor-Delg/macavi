a3bde556b36f2ba5f7cac7e3dc31a52d1e22b374
Jose Manuel Sanchez Osma <josmsanchez11@misena.edu.co>
2022-03-22 22:38:48 -0500

    |

--- MapeoDB/macavi/src/main/java/com/calzadomacavi/macavi/mapeo/domain/usuario.java
+++ MapeoDB/macavi/src/main/java/com/calzadomacavi/macavi/mapeo/domain/usuario.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 
 @Entity
 @Table (name = "usuario",schema = "mapeo", uniqueConstraints = {
-        @UniqueConstraint(name = "uk_dni", columnNames = {"id_dni","numero_dni"}),
+        @UniqueConstraint(name = "uk_dni", columnNames = {"id_tipo_dni","numero_dni"}),
         @UniqueConstraint(name = "uk_usuario_login", columnNames = "login_usuario"),
         @UniqueConstraint(name = "uk_email", columnNames = "email")
 
@@ -26,9 +26,59 @@ public class usuario {
     private String Email;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "id_dni",nullable = false, referencedColumnName = "id",foreignKey = @ForeignKey(name = "fk_usu_dni"))
+    @JoinColumn(name = "id_tipo_dni",nullable = false, referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "fk_usu_dni"))
 
-    private TipoDni dni;
 
+    public void setId(long id) {
+        Id = id;
+    }
 
+    public String getNumeroDni() {
+        return NumeroDni;
+    }
+
+    public void setNumeroDni(String numeroDni) {
+        NumeroDni = numeroDni;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getLoginUsuario() {
+        return LoginUsuario;
+    }
+
+    public void setLoginUsuario(String loginUsuario) {
+        LoginUsuario = loginUsuario;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public TipoDni getTipoDni() {
+        return tipoDni;
+    }
+
+    public void setTipoDni(TipoDni tipoDni) {
+        this.tipoDni = tipoDni;
+    }
 }
