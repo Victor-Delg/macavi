d7062959ae4e51d4f52fec213d9d30517ef8159b
Victor-Delg <Vicsdelgado@misena.edu.co>
2022-03-22 20:58:02 -0500

    mapeo

---
+++ MapeoDB/macavi/src/main/java/com/calzadomacavi/macavi/MacaviApplication.java
@@ -0,0 +1,13 @@
+package com.calzadomacavi.macavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacaviApplication.class, args);
+	}
+
+}
---
+++ MapeoDB/macavi/src/test/java/com/calzadomacavi/macavi/MacaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calzadomacavi.macavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
