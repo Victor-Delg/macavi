f336c7303758c7b110fbe0d7eb700fa091a251ef
Victor-Delg <Vicsdelgado@misena.edu.co>
2022-03-22 21:36:19 -0500

    update

--- MapeoDB/macavi/src/main/java/com/calzadomacavi/macavi/mapeo/domain/rol.java
+++ MapeoDB/macavi/src/main/java/com/calzadomacavi/macavi/mapeo/domain/rol.java
@@ -1,4 +1,16 @@
 package com.calzadomacavi.macavi.mapeo.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "rol",schema="mapeo", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_rol",columnNames = "nombre_rol")}
+)
+
 public class rol {
+    @Id
+    private long id;
+    @Column(name = "nombre_rol", length = 20, nullable = false)
+    private String nombreRol;
+
 }
